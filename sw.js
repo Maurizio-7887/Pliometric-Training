91435aa15f83ace4edece7946a37c45be9a7e042
Tasklet <tasklet@users.noreply.github.com>
2026-08-22 08:41:59 +0000

    Deploy layout video e musica dopo countdown

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'scatto-forza-30-v9-video-fullscreen';
+const CACHE = 'scatto-forza-30-v10-video-layout-spotify';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
