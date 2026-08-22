7a29f18fd7e55ffacaf9f88bd52e2dbb0f7763db
Tasklet <tasklet@users.noreply.github.com>
2026-08-22 08:35:32 +0000

    Video completi e visibili a schermo intero

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'scatto-forza-30-v8-mobile-video-timer';
+const CACHE = 'scatto-forza-30-v9-video-fullscreen';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
