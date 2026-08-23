f55c4899381e70b1faf0a65f3945ad14d2c15d27
Maurizio-7887 <gustinicchimaurizio@gmail.com>
2026-08-23 12:37:56 +0200

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'scatto-forza-30-v18-run-pace-log-reset';
+const CACHE = 'scatto-forza-30-v19-fixed-runs-wake-lock';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
